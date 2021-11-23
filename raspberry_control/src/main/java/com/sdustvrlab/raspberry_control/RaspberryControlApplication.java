bf0316cd18cb629f6700048a63f697836d95f887
sdustvrlab <94876802+sdustvrlab@users.noreply.github.com>
2021-11-23 09:29:45 +0800

    a springboot project

---
+++ raspberry_control/src/main/java/com/sdustvrlab/raspberry_control/RaspberryControlApplication.java
@@ -0,0 +1,11 @@
+package com.sdustvrlab.raspberry_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryControlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RaspberryControlApplication.class, args);
+    }
+}
---
+++ raspberry_control/src/main/java/com/sdustvrlab/raspberry_control/bean/Response.java
@@ -0,0 +1,49 @@
+package com.sdustvrlab.raspberry_control.bean;
+
+public class Response<T> {
+    private String status;
+    private String message;
+    private T result;
+
+    public Response() {
+    }
+
+    public Response(String status, String message, T result) {
+        this.status = status;
+        this.message = message;
+        this.result = result;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "status='" + status + '\'' +
+                ", message='" + message + '\'' +
+                ", result=" + result +
+                '}';
+    }
+}
